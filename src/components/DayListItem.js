a9a1314098c00142c96322e52772c911dee6d636
remained3 <sarahthebush@hotmail.com>
2021-12-06 11:15:12 -0500

    DayListItem styled and working in storybook

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,27 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = function (props) {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    } else if (props.spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return props.spots + " spots remaining";
+    }  
+  };
+
+  const dayClass = classNames("day-list__item",{
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} data-testid="day">
+      <h2>{props.name}</h2>
+      <h3> {formatSpots(props)}</h3>
     </li>
   );
 }
