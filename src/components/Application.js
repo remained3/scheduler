81f23873b80e7f86ca4d0c4ebaf23cad739ab019
remained3 <sarahthebush@hotmail.com>
2021-12-13 15:29:29 -0500

    added appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import InterviewerList from "./InterviewerList";
 
 const days = [
     {
@@ -20,6 +21,21 @@ const days = [
     },
   ];
 
+  const interviewer = {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png"
+  
+  };
+  
+  const interviewers = [
+    { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+    { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+    { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+    { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+    { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+  ];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   return (
@@ -28,6 +44,7 @@ export default function Application(props) {
       <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu"><DayList days={days} value={day} onChange={setDay} /></nav>
+      <InterviewerList interviewers={interviewers} setInterviewer = {console.log} interviewer={interviewer.id} />
       <img
         className="sidebar__lhl sidebar--centered"
         src="images/lhl.png"
---
+++ src/components/Appointment/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+
+export default function Appointment(props) {
+  const apptTime = () => {
+    if (!props.time) {
+      return "No appointments"
+    } else {
+      return `Appointment at ${props.time}`
+    }
+  }
+
+   return (
+<article className="appointment">{apptTime()}</article>
+   );
+ }
