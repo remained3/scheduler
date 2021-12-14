40d9ab73989ff1fd5e9998a5986c8ef658c12188
remained3 <sarahthebush@hotmail.com>
2021-12-14 10:13:55 -0500

    add onSubmit to form

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,20 +1,20 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
-import "components/InterviewerList.scss";
+import "./InterviewerList.scss";
 
 export default function InterviewerList(props) {
 
-  const parsedInterviewers = props.interviewers.map((interviewer) => {
-    return (
+  const parsedInterviewers = props.interviewers.map((interviewer) => 
+    
     <InterviewerListItem
     key = {interviewer.id}
     name = {interviewer.name}
     avatar = {interviewer.avatar}
-    selected={interviewer.id === props.interviewer}
-    setInterviewer={() => props.setInterviewer(interviewer.id)}
+    selected={interviewer.id === props.value}
+    setInterviewer={() => props.onChange(interviewer.id)}
     />
   );
-    });
+  
   
   return (<section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
