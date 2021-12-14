f8d123a6aeae1fc2e1ecb51413b6a3552cd34cfc
remained3 <sarahthebush@hotmail.com>
2021-12-14 10:45:30 -0500

    updating app and fixed interviewer select bug

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -5,8 +5,7 @@ import classNames from "classnames";
 export default function InterviewerListItem(props) {
   let interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
 
-  return (
-  <li className={interviewerClass} onClick={() => props.setInterviewer}>
+  return ( <li className={interviewerClass} onClick={props.setInterviewer}>
     <img
       className="interviewers__item-image"
       src={props.avatar}
