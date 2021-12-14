87bc6c083ee714933ef5ab53cbcf140f4b040b87
remained3 <sarahthebush@hotmail.com>
2021-12-14 10:28:01 -0500

    updated appointment/index

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, {Fragment} from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const apptTime = () => {
-    if (!props.time) {
-      return "No appointments"
-    } else {
-      return `Appointment at ${props.time}`
-    }
-  }
-  return <article className="appointment">{apptTime()}</article>
+  return (<article className="appointment">
+    <Header time={props.time} />
+    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+  </article>)
 }
