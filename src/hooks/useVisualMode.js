9ce3fe6d2031fcb0aeeba0b1f27978f3e3c1ba02
remained3 <sarahthebush@hotmail.com>
2021-12-15 15:42:11 -0500

    custom hook useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,28 @@ import React, {useState} from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial); 
-  return {mode}
+  const [history, setHistory] = useState([initial]);
+
+  function transition (newMode, replace = false) {
+    setMode(newMode);
+    if (replace === true) {
+      let newHistory = [...history];
+      newHistory[history.length - 1] = newMode;
+      setHistory(newHistory);
+    } else {
+      let newHistory = [...history, newMode];
+      setHistory(newHistory);
+    }
+  }
+
+  function back () {
+    if (history.length > 1) {
+      let newHistory = [...history];
+      newHistory.pop();
+      setMode(history[history.length - 2]);
+      setHistory(newHistory);
+    }
+  }
+
+  return { mode, transition, back };
 }
